03e317e07dc28a8622c6eda513d29b79322a30c8
Neil Sequeira <subs@thesequeirafamily.com>
2020-06-05 20:42:50 +1000

    first commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 # meters
+bounce = 1
+const = 0.6
+
+while bounce < 11:
+    height = round(height * const, 4)
+    bounce = bounce + 1
+    print (bounce, height)
